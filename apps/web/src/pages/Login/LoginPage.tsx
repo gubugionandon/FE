617457d92de3512ba5ee76e855c8b0e1077b270f
Lee hwan seuk <sioo1014@naver.com>
2025-10-21 10:37:23 +0900

    fix: 폴더 구조 수정

--- apps/web/src/pages/Login/LoginPage.tsx
+++ apps/web/src/pages/Login/LoginPage.tsx
@@ -1,5 +1,15 @@
+import HeroSection from './components/HeroSection';
+
 const LoginPage = () => {
-  return <div>로그인</div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <div className="relative w-full overflow-visible md:mx-auto md:max-w-screen-lg md:px-10">
+        <section className="flex h-screen w-full flex-col items-center justify-center gap-10 md:gap-[50px]">
+          <HeroSection />
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default LoginPage;
---
+++ apps/web/src/pages/Login/components/HeroSection.tsx
@@ -0,0 +1,9 @@
+export default function HeroSection() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center">
+        <div className="text-center"></div>
+      </div>
+    </div>
+  );
+}
