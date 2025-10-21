0d415fba1fb457a2e891d3a1177455a50d54fe2b
Lee hwan seuk <sioo1014@naver.com>
2025-10-21 17:20:19 +0900

    feat(auth): 로그인 ui 임시 구현

--- apps/web/src/pages/Login/components/HeroSection.tsx
+++ apps/web/src/pages/Login/components/HeroSection.tsx
@@ -1,9 +1,15 @@
 export default function HeroSection() {
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex flex-col items-center">
-        <div className="text-center"></div>
+    <div className="flex flex-col items-center gap-4">
+      <div className="flex flex-row items-center gap-5">
+        <span className="aspect -[37/32] h-[64px] w-[74px] bg-[rgba(255,191,0,0.10)]" />
+        <span className="text-grey-600 text-[44px] font-extrabold">
+          거부기린
+        </span>
       </div>
+      <p className="text-body-lg-medium text-grey-400">
+        세상 모든 거북목들이 기린이 될 때까지
+      </p>
     </div>
   );
 }
