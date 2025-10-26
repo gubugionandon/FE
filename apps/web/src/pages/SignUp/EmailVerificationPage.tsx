d614c62f03f67087a1672bfadfc7abcb89421ec4
Lee hwan seuk <sioo1014@naver.com>
2025-10-26 23:16:01 +0900

    fix(verify): 이메일 인증 페이지 스타일 수정

--- apps/web/src/pages/SignUp/EmailVerificationPage.tsx
+++ apps/web/src/pages/SignUp/EmailVerificationPage.tsx
@@ -1,11 +1,22 @@
+import { Button } from '@ui/Button/Button';
 import HeroSection from './components/HeroSection';
 
 const EmailVerificationPage = () => {
   return (
-    <main className="flex flex-col items-center">
+    <main className="hbp:min-h-[calc(100vh-75px)] flex min-h-[calc(100vh-60px)] flex-col items-center justify-center">
       <div className="hbp:mx-auto hbp:max-w-screen-lg hbp:px-10 relative w-full overflow-visible">
-        <section className="hbp:gap-15 hbp:px-20 hbp:h-[calc(100vh-75px)] hbp:gap-15 flex h-[calc(100vh-60px)] w-full flex-col items-center justify-center gap-12 px-7">
+        <section className="= flex w-full flex-col items-center justify-center gap-12 px-7">
           <HeroSection />
+          <Button
+            text="로그인"
+            className="text-body-xl-medium mb-[-16px] h-[49px] w-[440px]"
+          />
+          <p className="text-caption-sm-regular text-grey-300 flex flex-row gap-4">
+            이메일을 못받으셨나요?
+            <span className="text-yellow-500 underline">
+              이메일 다시 보내기
+            </span>
+          </p>
         </section>
       </div>
     </main>
--- apps/web/src/pages/SignUp/components/HeroSection.tsx
+++ apps/web/src/pages/SignUp/components/HeroSection.tsx
@@ -2,9 +2,9 @@ import EmailIcon from '@assets/auth/email_icon.svg?react';
 
 export default function HeroSection() {
   return (
-    <div className="hbp:gap-[57px] flex flex-col items-center gap-[46px]">
-      <EmailIcon className="hbp:w-[250px] hbp:h-[250px]" />
-      <div className="hbp:gap-7.5 flex flex-col items-center justify-center gap-6">
+    <div className="flex flex-col items-center gap-[46px]">
+      <EmailIcon className="ml-5" />
+      <div className="flex flex-col items-center justify-center gap-6">
         <p className="text-title-4xl-bold">이메일 인증</p>
         <p className="text-headline-2xl-regular text-center">
           본인 인증 메일을 귀하의
